5e36d5ec58a9ac0a7f21faddf11d5e4491436c21
Alexandre Crettet <alexandre@cheerz.com>
2018-07-26 16:16:13 +0200

    Fix

--- app/src/main/java/com/cheerz/casadelrestau/login/signUp/SignUpModel.kt
+++ app/src/main/java/com/cheerz/casadelrestau/login/signUp/SignUpModel.kt
@@ -1,9 +1,12 @@
 package com.cheerz.casadelrestau.login.signUp
 
-class SignUpModel() : Login.Model {
+class SignUpModel : Login.Model {
+
+    override fun login(email: String, password: String) {
+        TODO()
+    }
 
     override fun signUp(email: String, password: String) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 }
