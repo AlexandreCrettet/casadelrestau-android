66d742f5f4e98bf8e05810a04e47836d6d5cff72
Alexandre Crettet <alexandre@cheerz.com>
2018-07-26 21:11:07 +0200

    add routes for places

--- app/src/main/java/com/cheerz/casadelrestau/network/CasaDelRestauService.kt
+++ app/src/main/java/com/cheerz/casadelrestau/network/CasaDelRestauService.kt
@@ -1,22 +1,31 @@
 package com.cheerz.casadelrestau.network
 
 import com.cheerz.casadelrestau.network.data.MiamzLogin
+import com.cheerz.casadelrestau.network.data.MiamzPlace
 import com.cheerz.casadelrestau.network.data.MiamzReqLogin
+import com.cheerz.casadelrestau.network.data.MiamzReqPlace
 import com.cheerz.casadelrestau.network.data.MiamzReqSignUp
 import com.cheerz.casadelrestau.network.data.MiamzSignUp
 import io.reactivex.Single
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface CasaDelRestauService {
 
     companion object {
-        const val BASE_URL = "https://floating-earth-31565.herokuapp.com/"
+        const val BASE_URL = "https://floating-earth-31565.herokuapp.com/api/1/"
     }
 
-    @POST("api/1/session")
+    @POST("session")
     fun login(@Body params: MiamzReqLogin): Single<MiamzLogin>
 
-    @POST("api/1/users")
+    @POST("users")
     fun signUp(@Body params: MiamzReqSignUp): Single<MiamzSignUp>
+
+    @POST("places")
+    fun postNewPlace(): Single<MiamzReqPlace>
+
+    @GET("places")
+    fun getPlaces(@Body place: MiamzPlace): Single<MiamzReqPlace>
 }
--- app/src/main/java/com/cheerz/casadelrestau/network/data/MiamzLogin.kt
+++ app/src/main/java/com/cheerz/casadelrestau/network/data/MiamzLogin.kt
@@ -8,3 +8,19 @@ data class MiamzSignUp(val email: String, val nickname: String)
 data class MiamzReqSignUp(val user: MiamzReqSignUpUser)
 data class MiamzReqSignUpUser(val email: String, val password: String)
 
+data class MiamzReqPlaceData(
+        val name: String,
+        val lng: Float,
+        val lat: Float,
+        val first_image_url: String,
+        val second_image_url: String,
+        val third_image_url: String,
+        val fourth_image_url: String,
+        val fifth_image_url: String,
+        val place_category_tag: String,
+        val user_emai: String
+)
+
+data class MiamzReqPlace(val place: MiamzReqPlaceData)
+
+data class MiamzPlace(val lat: Float, val lng: Float, val meter_distance: Int)
