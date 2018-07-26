d672ca450b8189ae37610e91b6234cccf6cbf6cd
Alexandre Crettet <alexandre@cheerz.com>
2018-07-26 12:46:02 +0200

    Add network client / service

---
+++ app/src/main/java/com/cheerz/casadelrestau/network/CasaDelRestauService.kt
@@ -0,0 +1,9 @@
+package com.cheerz.casadelrestau.network
+
+interface CasaDelRestauService {
+
+    companion object {
+        const val BASE_URL = "https://www.TODO"
+    }
+
+}
---
+++ app/src/main/java/com/cheerz/casadelrestau/network/HttpClient.kt
@@ -0,0 +1,32 @@
+package com.cheerz.casadelrestau.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object HttpClient {
+
+    val service: CasaDelRestauService by lazy { buildRadioKingService() }
+
+    private fun buildRadioKingService(): CasaDelRestauService {
+        val client = buildRadioKingClient()
+        return Retrofit.Builder()
+                .baseUrl(CasaDelRestauService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
+                .build()
+                .create(CasaDelRestauService::class.java)
+    }
+
+    private fun buildRadioKingClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+    }
+}
