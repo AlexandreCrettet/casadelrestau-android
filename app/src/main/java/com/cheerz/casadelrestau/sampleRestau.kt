466a3fcd02639c5ef420009d204bc9cacb7b6598
Alexandre Crettet <alexandre@cheerz.com>
2018-07-27 05:26:18 +0200

    fix fake data

--- app/src/main/java/com/cheerz/casadelrestau/sampleRestau.kt
+++ app/src/main/java/com/cheerz/casadelrestau/sampleRestau.kt
@@ -6,11 +6,11 @@ object sampleRestau {
 
     fun getSample(): List<MiamzReqPlaceData> {
         val user  = listOf<String>("jean","mathieu", "michael", "toto", "tutu" )
-        val test = MiamzReqPlaceData("nom", 12.0, 12.0, "path", "path2", "path",
+        val test = MiamzReqPlaceData(0, "nom", 12.0, 12.0, "path", "path2", "path",
             "path", "path", "Categorie", "User email", "12:30 PM - 14:00",user )
-        val test2 = MiamzReqPlaceData("nom2", 12.0, 12.0, "path", "path2", "path",
+        val test2 = MiamzReqPlaceData(1, "nom2", 12.0, 12.0, "path", "path2", "path",
             "path", "path", "Categorie2", "User email2","12:30 PM - 14:00", user )
-        val test3 = MiamzReqPlaceData("nom3", 12.0, 12.0, "path", "path2", "path",
+        val test3 = MiamzReqPlaceData(2, "nom3", 12.0, 12.0, "path", "path2", "path",
             "path", "path", "Categorie3", "User email3", "12:30 PM - 14:00", user)
         val places: MutableList<MiamzReqPlaceData> = mutableListOf(test, test2, test3)
         return places
